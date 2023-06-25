24203c84bd589d2f8be04c5f0cdb691cc6e94e13
Gamotto <112080813+Gamotto@users.noreply.github.com>
2023-06-25 17:32:47 -0500

    Update nv-Tengo Miedo.js

--- plugins/nv-Tengo Miedo.js
+++ plugins/nv-Tengo Miedo.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/Tengo Miedo.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
-conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `tengo miedo.mp3` }, { quoted: m })}
+conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `Tengo Miedo.mp3` }, { quoted: m })}
 handler.customPrefix = /tengo miedo|TENGO MIEDO|Tengo miedo/
 handler.command = /^(tengo miedo|TENGO MIEDO|Tengo miedo?$)/
 export default handler
