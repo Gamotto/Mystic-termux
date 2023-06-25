559858f7e858a4be0e03b526411c4fa5a03c4ace
Gamotto <112080813+Gamotto@users.noreply.github.com>
2023-06-25 17:14:56 -0500

    Update and rename nv-corre.js to nv-tengo miedo.js

--- plugins/nv-tengo miedo.js
+++ plugins/nv-tengo miedo.js
@@ -1,19 +1,19 @@
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/corre.mp3'
+let vn = './media/tengo miedo.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
-conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `corre.mp3` }, { quoted: m })}
-handler.customPrefix = /corre|CORRE|Corre/
-handler.command = /^(corre|CORRE|Corre?$)/
+conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `tengo miedo.mp3` }, { quoted: m })}
+handler.customPrefix = /tengo miedo|TENGO MIEDO|Tengo miedo/
+handler.command = /^(tengo miedo|TENGO MIEDO|Tengo miedo?$)/
 export default handler
 
 /*
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/corre.mp3'
+let vn = './media/tengo miedo.mp3'
 conn.sendPresenceUpdate('recording', m.chat)  
 conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `👑 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭 👑`, "body": `=> ᴀᴜᴅɪᴏ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴏ`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://github.com/BrunoSobrino/TheMystic-Bot-MD`, "showAdAttribution": true}}, seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
-handler.customPrefix = /corre|CORRE|Corre/
-handler.command = /^(corre|CORRE|Corre?$)/
+handler.customPrefix = /tengo miedo|TENGO MIEDO|Tengo miedo/
+handler.command = /^(tengo miedo|TENGO MIEDO|Tengo miedo?$)/
 export default handler
 */
