6837a5d1b9b9dbd66438b6a10d7a5bebc30be2c2
Gamotto <112080813+Gamotto@users.noreply.github.com>
2023-06-25 17:24:44 -0500

    Update and rename nv-tengo miedo.js to nv-Tengo Miedo.js

--- plugins/nv-Tengo Miedo.js
+++ plugins/nv-Tengo Miedo.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/tengo miedo.mp3'
+let vn = './media/Tengo Miedo.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
 conn.sendMessage(m.chat, { audio: { url: vn }, seconds: '1934.4', ptt: true, mimetype: 'audio/mpeg', fileName: `tengo miedo.mp3` }, { quoted: m })}
 handler.customPrefix = /tengo miedo|TENGO MIEDO|Tengo miedo/
@@ -10,7 +10,7 @@ export default handler
 /*
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/tengo miedo.mp3'
+let vn = './media/Tengo Miedo.mp3'
 conn.sendPresenceUpdate('recording', m.chat)  
 conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `👑 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭 👑`, "body": `=> ᴀᴜᴅɪᴏ ᴀᴜᴛᴏᴍᴀᴛɪᴄᴏ`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://github.com/BrunoSobrino/TheMystic-Bot-MD`, "showAdAttribution": true}}, seconds: '4556', ptt: true, mimetype: 'audio/mpeg', fileName: `error.mp3` }, { quoted: m })}
 handler.customPrefix = /tengo miedo|TENGO MIEDO|Tengo miedo/
